d0cee588078145b9f37a7908218f2732dd5b0546
abdulmunaf <abdulmunaf380@gmail.com>
2022-01-23 19:50:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+
+import React, { useState } from 'react'
+import NavBar from './components/NavBar';
+import News from './components/News';
+import {HashRouter,Switch,Route} from "react-router-dom" 
+import LoadingBar from 'react-top-loading-bar'
+
+const App=()=> {
+ const pageSize = 6;
+  const apiKey = process.env.REACT_APP_NEWS_API
+
+  const [Progress, setProgress] = useState(0);
+
+  
+ 
+    return (
+      <div>
+        <HashRouter basename={process.env.PUBLIC_URL}>
+       <NavBar/>
+       <LoadingBar
+        color='#f11946'
+        height= {6}
+        progress={Progress}
+      />
+       <Switch>
+       <Route exact path="/"><News setProgress ={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general"/></Route>
+       <Route exact path="/business"><News setProgress ={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country="in" category="business"/></Route>
+       <Route exact path="/entertainment"><News setProgress ={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country="in" category="entertainment"/></Route>
+      
+       <Route exact path="/health"><News setProgress ={setProgress} apiKey={apiKey} key="health" pageSize={pageSize} country="in" category="health"/></Route>
+       <Route exact path="/science"><News setProgress ={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country="in" category="science"/></Route>
+       <Route exact path="/sports"><News setProgress ={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country="in" category="sports"/></Route>
+       <Route exact path="/technology"><News setProgress ={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country="in" category="technology"/></Route>
+       
+       </Switch>
+       </HashRouter>
+      </div>
+    )
+  }
+
+export default App;
+
